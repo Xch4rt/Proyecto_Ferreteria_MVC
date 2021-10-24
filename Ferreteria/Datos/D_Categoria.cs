7245d63c108904c008a0ed665da40293e52c4877
Xch4rt <pab203.guti@gmail.com>
2021-10-23 23:29:45 -0600

    configurando conexion en App.config y creando clase D_Conexion

---
+++ Ferreteria/Datos/D_Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Datos
+{
+    public class D_Categoria
+    {
+        SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conectar"].ConnectionString);
+    }
+}
