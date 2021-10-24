d212aeea304e65c7c66b0a8b8c8eebe243ea7dc2
Xch4rt <pab203.guti@gmail.com>
2021-10-23 22:42:22 -0600

    Iniciando con los querys para la Ferreteria

---
+++ Query_Ferreteria.sql
@@ -0,0 +1,13 @@
+create database Mantenimiento_Productos
+
+use Mantenimiento_Productos
+
+-- Creando Tablas: Categorias
+create table Categoria(
+	IdCategoria int identity (1,1) primary key not null,
+	Codigo as ('CT'+RIGHT('00'+CONVERT(varchar, IdCategoria),(2))), -- formato CT01, CT02, CT03
+	Nombre nvarchar (30) not null,
+	Descripcion nvarchar(256) null
+)
+SELECT * FROM Categoria
+insert into Categoria values('PINTURERIA','LAS MEJORES PINTURAS PARA TU CASA')
---
+++ Query_SP.sql
@@ -0,0 +1,33 @@
+-- Procedimiento almacendo para buscar categorias
+
+create proc SP_BuscarCategoria
+@Buscar nvarchar(20)
+as 
+select * from Categoria
+where Nombre like  @Buscar + '%'
+
+-- Procedimiento almacenado para insertar categoria
+create proc SP_InsertarCategoria
+@Nombre nvarchar(30),
+@Descripcion nvarchar(256)
+as 
+insert into Categoria values (@Nombre, @Descripcion)
+
+
+-- Procedimiento almacenado para editar categoria
+
+create proc SP_EditarCategoria
+@IdCategoria int,
+@Nombre nvarchar(30),
+@Descripcion nvarchar (256)
+as 
+update Categoria set Nombre = @Nombre, Descripcion = @Descripcion
+where IdCategoria = @IdCategoria
+
+
+-- Procedimiento almacenado para eliminar categoria
+create proc SP_EliminarCategoria
+@IdCategoria int
+as 
+delete Categoria 
+where IdCategoria = @IdCategoria
