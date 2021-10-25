e7024b3355ad183d4d73520121ebac0da9c8a9f9
Xch4rt <pab203.guti@gmail.com>
2021-10-24 20:14:52 -0600

    Creando N_Categoria para la comunicacion entre objetos

---
+++ Ferreteria/Negocios/N_Categoria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+using Datos;
+namespace Negocios
+{
+    public class N_Categoria
+    {
+        D_Categoria objData = new D_Categoria();
+
+        public List<E_Categorias> listandoCategoria(string buscar)
+        {
+            return objData.ListarCategorias(buscar);
+        }
+
+        public void insertandoCategoria(E_Categorias Categoria)
+        {
+            objData.insertarCategoria(Categoria);
+        }
+        
+        public void editandoCategoria(E_Categorias Categoria)
+        {
+            objData.EditarCategoria(Categoria);
+        }
+
+        public void eliminandoCategoria(E_Categorias Categoria)
+        {
+            objData.EliminarCategoria(Categoria);
+        }
+    }
+}
