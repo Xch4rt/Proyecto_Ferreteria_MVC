406cb3e043891d6528db63d905e5e387192e9ac7
Xch4rt <pab203.guti@gmail.com>
2021-11-04 23:27:14 -0700

    Creando clases Usuarios en las diferentes capas para la creacion de usuarios

---
+++ Ferreteria/Datos/D_Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Configuration;
+using Entidades;
+using System.Data;
+
+namespace Datos
+{
+    public class D_Usuarios
+    {
+        SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conectar"].ConnectionString);
+
+        
+    }
+}
---
+++ Ferreteria/Entidades/E_Usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class E_Usuarios
+    {
+        private string Username;
+        private string Password;
+
+        public string Username1 { get => Username; set => Username = value; }
+        public string Password1 { get => Password; set => Password = value; }
+    }
+}
