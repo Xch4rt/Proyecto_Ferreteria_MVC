200de305ca25c783a001a05c6f2597bbf179de33
Xch4rt <pab203.guti@gmail.com>
2021-12-02 21:29:50 -0700

    Ojala funcione dale proba'

--- Ferreteria/Presentacion/FrmDashboard.Designer.cs
+++ Ferreteria/Presentacion/FrmDashboard.Designer.cs
@@ -29,20 +29,210 @@ namespace Presentacion
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new Bunifu.UI.WinForms.BunifuShadowPanel();
+            this.label2 = new Bunifu.UI.WinForms.BunifuShadowPanel();
+            this.label3 = new Bunifu.UI.WinForms.BunifuShadowPanel();
+            this.bunifuDatavizBasic1 = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic();
+            this.bunifuDatavizBasic2 = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic();
+            this.bunifuShadowPanel1 = new Bunifu.UI.WinForms.BunifuShadowPanel();
+            this.bunifuDatavizBasic3 = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic();
+            this.label1.SuspendLayout();
+            this.label2.SuspendLayout();
+            this.label3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.BorderRadius = 1;
+            this.label1.BorderThickness = 1;
+            this.label1.Controls.Add(this.bunifuDatavizBasic2);
+            this.label1.FillStyle = Bunifu.UI.WinForms.BunifuShadowPanel.FillStyles.Solid;
+            this.label1.GradientMode = Bunifu.UI.WinForms.BunifuShadowPanel.GradientModes.Vertical;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.PanelColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.PanelColor2 = System.Drawing.Color.WhiteSmoke;
+            this.label1.ShadowColor = System.Drawing.Color.DarkGray;
+            this.label1.ShadowDept = 2;
+            this.label1.ShadowDepth = 5;
+            this.label1.ShadowStyle = Bunifu.UI.WinForms.BunifuShadowPanel.ShadowStyles.Surrounded;
+            this.label1.ShadowTopLeftVisible = false;
+            this.label1.Size = new System.Drawing.Size(300, 308);
+            this.label1.Style = Bunifu.UI.WinForms.BunifuShadowPanel.BevelStyles.Flat;
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.BorderRadius = 1;
+            this.label2.BorderThickness = 1;
+            this.label2.Controls.Add(this.bunifuDatavizBasic1);
+            this.label2.FillStyle = Bunifu.UI.WinForms.BunifuShadowPanel.FillStyles.Solid;
+            this.label2.GradientMode = Bunifu.UI.WinForms.BunifuShadowPanel.GradientModes.Vertical;
+            this.label2.Location = new System.Drawing.Point(318, 12);
+            this.label2.Name = "label2";
+            this.label2.PanelColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.PanelColor2 = System.Drawing.Color.WhiteSmoke;
+            this.label2.ShadowColor = System.Drawing.Color.DarkGray;
+            this.label2.ShadowDept = 2;
+            this.label2.ShadowDepth = 5;
+            this.label2.ShadowStyle = Bunifu.UI.WinForms.BunifuShadowPanel.ShadowStyles.Surrounded;
+            this.label2.ShadowTopLeftVisible = false;
+            this.label2.Size = new System.Drawing.Size(714, 308);
+            this.label2.Style = Bunifu.UI.WinForms.BunifuShadowPanel.BevelStyles.Flat;
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.BorderRadius = 1;
+            this.label3.BorderThickness = 1;
+            this.label3.Controls.Add(this.bunifuDatavizBasic3);
+            this.label3.FillStyle = Bunifu.UI.WinForms.BunifuShadowPanel.FillStyles.Solid;
+            this.label3.GradientMode = Bunifu.UI.WinForms.BunifuShadowPanel.GradientModes.Vertical;
+            this.label3.Location = new System.Drawing.Point(12, 326);
+            this.label3.Name = "label3";
+            this.label3.PanelColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.PanelColor2 = System.Drawing.Color.WhiteSmoke;
+            this.label3.ShadowColor = System.Drawing.Color.DarkGray;
+            this.label3.ShadowDept = 2;
+            this.label3.ShadowDepth = 5;
+            this.label3.ShadowStyle = Bunifu.UI.WinForms.BunifuShadowPanel.ShadowStyles.Surrounded;
+            this.label3.ShadowTopLeftVisible = false;
+            this.label3.Size = new System.Drawing.Size(595, 351);
+            this.label3.Style = Bunifu.UI.WinForms.BunifuShadowPanel.BevelStyles.Flat;
+            this.label3.TabIndex = 2;
+            // 
+            // bunifuDatavizBasic1
+            // 
+            this.bunifuDatavizBasic1.animationEnabled = false;
+            this.bunifuDatavizBasic1.AxisLineColor = System.Drawing.Color.LightGray;
+            this.bunifuDatavizBasic1.AxisXFontColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic1.AxisXGridColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic1.AxisXGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bunifuDatavizBasic1.AxisYFontColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic1.AxisYGridColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic1.AxisYGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bunifuDatavizBasic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuDatavizBasic1.Location = new System.Drawing.Point(3, 3);
+            this.bunifuDatavizBasic1.Name = "bunifuDatavizBasic1";
+            this.bunifuDatavizBasic1.Size = new System.Drawing.Size(714, 308);
+            this.bunifuDatavizBasic1.TabIndex = 0;
+            this.bunifuDatavizBasic1.Theme = Bunifu.Dataviz.WinForms.BunifuDatavizBasic._theme.theme1;
+            this.bunifuDatavizBasic1.Title = "";
+            // 
+            // bunifuDatavizBasic2
+            // 
+            this.bunifuDatavizBasic2.animationEnabled = false;
+            this.bunifuDatavizBasic2.AxisLineColor = System.Drawing.Color.LightGray;
+            this.bunifuDatavizBasic2.AxisXFontColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic2.AxisXGridColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic2.AxisXGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bunifuDatavizBasic2.AxisYFontColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic2.AxisYGridColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic2.AxisYGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bunifuDatavizBasic2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuDatavizBasic2.Location = new System.Drawing.Point(3, 3);
+            this.bunifuDatavizBasic2.Name = "bunifuDatavizBasic2";
+            this.bunifuDatavizBasic2.Size = new System.Drawing.Size(300, 308);
+            this.bunifuDatavizBasic2.TabIndex = 0;
+            this.bunifuDatavizBasic2.Theme = Bunifu.Dataviz.WinForms.BunifuDatavizBasic._theme.theme1;
+            this.bunifuDatavizBasic2.Title = "";
+            // 
+            // bunifuShadowPanel1
+            // 
+            this.bunifuShadowPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuShadowPanel1.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.bunifuShadowPanel1.BorderRadius = 1;
+            this.bunifuShadowPanel1.BorderThickness = 1;
+            this.bunifuShadowPanel1.FillStyle = Bunifu.UI.WinForms.BunifuShadowPanel.FillStyles.Solid;
+            this.bunifuShadowPanel1.GradientMode = Bunifu.UI.WinForms.BunifuShadowPanel.GradientModes.Vertical;
+            this.bunifuShadowPanel1.Location = new System.Drawing.Point(613, 326);
+            this.bunifuShadowPanel1.Name = "bunifuShadowPanel1";
+            this.bunifuShadowPanel1.PanelColor = System.Drawing.Color.WhiteSmoke;
+            this.bunifuShadowPanel1.PanelColor2 = System.Drawing.Color.WhiteSmoke;
+            this.bunifuShadowPanel1.ShadowColor = System.Drawing.Color.DarkGray;
+            this.bunifuShadowPanel1.ShadowDept = 2;
+            this.bunifuShadowPanel1.ShadowDepth = 5;
+            this.bunifuShadowPanel1.ShadowStyle = Bunifu.UI.WinForms.BunifuShadowPanel.ShadowStyles.Surrounded;
+            this.bunifuShadowPanel1.ShadowTopLeftVisible = false;
+            this.bunifuShadowPanel1.Size = new System.Drawing.Size(419, 351);
+            this.bunifuShadowPanel1.Style = Bunifu.UI.WinForms.BunifuShadowPanel.BevelStyles.Flat;
+            this.bunifuShadowPanel1.TabIndex = 3;
+            // 
+            // bunifuDatavizBasic3
+            // 
+            this.bunifuDatavizBasic3.animationEnabled = false;
+            this.bunifuDatavizBasic3.AxisLineColor = System.Drawing.Color.LightGray;
+            this.bunifuDatavizBasic3.AxisXFontColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic3.AxisXGridColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic3.AxisXGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bunifuDatavizBasic3.AxisYFontColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic3.AxisYGridColor = System.Drawing.Color.Gray;
+            this.bunifuDatavizBasic3.AxisYGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bunifuDatavizBasic3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuDatavizBasic3.Location = new System.Drawing.Point(3, 3);
+            this.bunifuDatavizBasic3.Name = "bunifuDatavizBasic3";
+            this.bunifuDatavizBasic3.Size = new System.Drawing.Size(595, 351);
+            this.bunifuDatavizBasic3.TabIndex = 0;
+            this.bunifuDatavizBasic3.Theme = Bunifu.Dataviz.WinForms.BunifuDatavizBasic._theme.theme1;
+            this.bunifuDatavizBasic3.Title = "";
             // 
             // FrmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1044, 689);
+            this.Controls.Add(this.bunifuShadowPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmDashboard";
             this.Text = "FrmDashboard";
+            this.label1.ResumeLayout(false);
+            this.label2.ResumeLayout(false);
+            this.label3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Bunifu.UI.WinForms.BunifuShadowPanel label1;
+        private Bunifu.UI.WinForms.BunifuShadowPanel label2;
+        private Bunifu.UI.WinForms.BunifuShadowPanel label3;
+        private Bunifu.Dataviz.WinForms.BunifuDatavizBasic bunifuDatavizBasic1;
+        private Bunifu.Dataviz.WinForms.BunifuDatavizBasic bunifuDatavizBasic2;
+        private Bunifu.UI.WinForms.BunifuShadowPanel bunifuShadowPanel1;
+        private Bunifu.Dataviz.WinForms.BunifuDatavizBasic bunifuDatavizBasic3;
     }
 }
--- Ferreteria/Presentacion/FrmDashboard.cs
+++ Ferreteria/Presentacion/FrmDashboard.cs
@@ -12,9 +12,67 @@ namespace Presentacion
 {
     public partial class FrmDashboard : Form
     {
+        Bunifu.Dataviz.WinForms.BunifuDatavizBasic.DataPoint data;
         public FrmDashboard()
         {
             InitializeComponent();
+            chartwlabel2();
+            chartwlabel1();
+            chartwlabel3();
+        }
+        private void chartwlabel2() // linear
+        {
+            data = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic.DataPoint(Bunifu.Dataviz.WinForms.BunifuDatavizBasic._type.Bunifu_line);
+            Bunifu.Dataviz.WinForms.BunifuDatavizBasic.Canvas canvas = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic.Canvas();
+            Random random = new Random();
+
+            data.addLabely("MON", random.Next(0, 50).ToString());
+            data.addLabely("TUE", random.Next(0, 50).ToString());
+            data.addLabely("WED", random.Next(0, 50).ToString());
+            data.addLabely("THU", random.Next(0, 50).ToString());
+            data.addLabely("FRI", random.Next(0, 50).ToString());
+            data.addLabely("SAT", random.Next(0, 50).ToString());
+            data.addLabely("SUN", random.Next(0, 50).ToString());
+
+
+            canvas.addData(data);
+            bunifuDatavizBasic1.Render(canvas);
+        }
+        private void chartwlabel1()
+        {
+            data = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic.DataPoint(Bunifu.Dataviz.WinForms.BunifuDatavizBasic._type.Bunifu_pie);
+            Bunifu.Dataviz.WinForms.BunifuDatavizBasic.Canvas canvas = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic.Canvas();
+            Random random = new Random();
+
+            data.addLabely("MON", random.Next(0, 50).ToString());
+            data.addLabely("TUE", random.Next(0, 50).ToString());
+            data.addLabely("WED", random.Next(0, 50).ToString());
+            data.addLabely("THU", random.Next(0, 50).ToString());
+            data.addLabely("FRI", random.Next(0, 50).ToString());
+            data.addLabely("SAT", random.Next(0, 50).ToString());
+            data.addLabely("SUN", random.Next(0, 50).ToString());
+
+
+            canvas.addData(data);
+            bunifuDatavizBasic2.Render(canvas);
+        }
+        private void chartwlabel3()
+        {
+            data = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic.DataPoint(Bunifu.Dataviz.WinForms.BunifuDatavizBasic._type.Bunifu_column);
+            Bunifu.Dataviz.WinForms.BunifuDatavizBasic.Canvas canvas = new Bunifu.Dataviz.WinForms.BunifuDatavizBasic.Canvas();
+            Random random = new Random();
+
+            data.addLabely("MON", random.Next(0, 50).ToString());
+            data.addLabely("TUE", random.Next(0, 50).ToString());
+            data.addLabely("WED", random.Next(0, 50).ToString());
+            data.addLabely("THU", random.Next(0, 50).ToString());
+            data.addLabely("FRI", random.Next(0, 50).ToString());
+            data.addLabely("SAT", random.Next(0, 50).ToString());
+            data.addLabely("SUN", random.Next(0, 50).ToString());
+
+
+            canvas.addData(data);
+            bunifuDatavizBasic3.Render(canvas);
         }
     }
 }
